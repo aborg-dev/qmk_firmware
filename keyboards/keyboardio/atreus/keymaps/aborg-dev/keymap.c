db52bb67984d26bb4a5dc78c0af8e0f3898b0181
Andrew Borg <git@aborg.dev>
2024-11-13 11:25:12 +0000

    Move backspace to a thumb

--- keyboards/keyboardio/atreus/keymaps/aborg-dev/keymap.c
+++ keyboards/keyboardio/atreus/keymaps/aborg-dev/keymap.c
@@ -33,7 +33,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_Q,    KC_W,    KC_E,    KC_R,           KC_T,                                                     KC_Y,           KC_U,           KC_I,    KC_O,    KC_P    ,
     KC_A,    KC_S,    KC_D,    CTL_T(KC_F),    KC_G,                                                     KC_H,           CTL_T(KC_J),    KC_K,    KC_L,    KC_SCLN ,
     KC_Z,    KC_X,    KC_C,    KC_V,           KC_B,              KC_GRV,             KC_BSLS,           KC_N,           KC_M,           KC_COMM, KC_DOT,  KC_SLSH ,
-    KC_ESC,  KC_TAB,  KC_LALT, MO(_MACRO),     LT(_NUM, KC_ENT), LM(_WM, MOD_LGUI),   LT(_NAV, KC_TAB),  SFT_T(KC_SPC),  KC_LANG,        CW_TOGG, OSL(_WM),KC_ENT ),
+    KC_ESC,  KC_TAB,  KC_LALT, MO(_MACRO),     LT(_NUM, KC_ENT), LM(_WM, MOD_LGUI),   LT(_NAV, KC_BSPC), SFT_T(KC_SPC),  KC_LANG,        CW_TOGG, OSL(_WM),KC_ENT ),
 
   /*
    *  ^       @      #     $    %        ||       *     7     8     9    +
